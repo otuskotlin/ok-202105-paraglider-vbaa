cc9ddae393374f052e307b643823b285023384b8
alexxxov1 <73472823+alexxxov1@users.noreply.github.com>
2021-09-20 01:02:34 +0300

    added custom date validation rule

--- paraglider-vbaa-be-logics/src/main/kotlin/ru/kotlin/paraglider/vbaa/be/logics/chains/school/validators/CommonSchoolValidators.kt
+++ paraglider-vbaa-be-logics/src/main/kotlin/ru/kotlin/paraglider/vbaa/be/logics/chains/school/validators/CommonSchoolValidators.kt
@@ -5,6 +5,7 @@ import io.konform.validation.jsonschema.minItems
 import ru.kotlin.paraglider.vbaa.be.common.context.SchoolContext
 import ru.kotlin.paraglider.vbaa.be.common.models.*
 import ru.kotlin.paraglider.vbaa.be.logics.validation.*
+import java.time.LocalDate
 
 fun schoolFieldsValidation() = Validation<SchoolModel> {
     SchoolModel::welcomeVideoUrl {
@@ -20,6 +21,11 @@ fun schoolFieldsValidation() = Validation<SchoolModel> {
         }
         InstructorModel::surname required {
             run(personNameCheck)
+        }
+        InstructorModel::dateOfBirth {
+            addConstraint("Год рождения должен быть больше 1920", LocalDate.of(1980,1,1).toString()) {
+                it.year > 1920
+            }
         }
     }
     SchoolModel::location required {
